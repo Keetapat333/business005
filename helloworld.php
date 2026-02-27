0eef4bc255c37f88a00912bfa62d1cbc9647a474
Keetapat333 <Keetapat369@gmail.com>
2026-02-27 11:35:30 +0700

    Add files via upload

---
+++ helloworld.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Keetapat</title>
+</head>
+
+<body>
+    Hello World fo PHP!!
+
+    <?php
+    echo "Hello World";
+    ?>
+</body>
+
+</html>
