dec5ebf5a6af96995f0eff12246ba045c8e2e595
Keetapat333 <Keetapat369@gmail.com>
2026-03-06 12:03:54 +0700

    upadate-first

--- select.php
+++ select.php
@@ -14,7 +14,7 @@
     use LDAP\Result;
 
     require "connect.php";
-    $sql = "SELECT * FROM customer";
+    $sql = "SELECT CustomerID, Name ,Email, CountryName FROM customer JOIN country ON customer.CountryCode = country.CountryCode   WHERE country.CountryCode = 'TH' AND customer.Email LIKE  '%@gmail.com'";
 
     $stmt = $conn->prepare($sql);
     $stmt->execute();
@@ -28,18 +28,18 @@
             <th width="140">
                 <div align=center>ชื่อ</div>
             </th>
-            <th width="120">
-                <div align=center>วันเกิด</div>
-            </th>
+            <!-- <th width="120"> -->
+            <!-- <div align=center>วันเกิด</div> -->
+            <!-- </th> -->
             <th width="100">
                 <div align=center>อีเมล์</div>
             </th>
             <th width="50">
                 <div align=center>ประเทศ</div>
             </th>
-            <th width="70">
+            <!-- <th width="70">
                 <div align=center>ยอดหนี้</div>
-            </th>
+            </th> -->
         </tr>
 
         <?php
@@ -56,12 +56,12 @@
                     <?php echo $result["Name"]; ?>
                 </td>
 
-                <td><?php echo $result["Birthdate"]; ?></div>
-                </td>
+                <!-- <td><?php echo $result["Birthdate"]; ?></div>
+                </td> -->
                 <td><?php echo $result["Email"]; ?></td>
-                <td><?php echo $result["CountryCode"]; ?></div>
+                <td><?php echo $result["CountryName"]; ?></div>
                 </td>
-                <td><?php echo $result["OutstandingDebt"]; ?></td>
+                <!-- <td><?php echo $result["OutstandingDebt"]; ?></td> -->
             </tr>
         <?php
         }
